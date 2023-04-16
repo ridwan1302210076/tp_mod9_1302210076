90f7944d2b03568e2907d00a627bee3db1c1e035
Ridwan NR <rahmannurridwan@student.telkomuniversity.ac.id>
2023-04-17 05:49:56 +0700

    Add project files.

---
+++ tp_mod9_1302210076/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace tp_mod9_1302210076
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
