23aaacb55725e346ea1a28d693cee983e468a10b
Ridwan NR <rahmannurridwan@student.telkomuniversity.ac.id>
2023-04-17 08:54:44 +0700

    Menambah Api mahasiswa

---
+++ tp_mod9_1302210076/Controllers/MahasiswaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace tp_mod9_1302210076.Controllers
+{
+    public class MahasiswaController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class mahasiswaController : ControllerBase
+        {
+            private static List<mahasiswa> data = new List<mahasiswa>
+        {
+            new mahasiswa("Ridwa Nur Rahman","1302210076"),
+            new mahasiswa("Hilmi Fauziyyah Supriatna ","1302210081"),
+            new mahasiswa("Ibrahim Mulyo Tsabit","1302210022"),
+            new mahasiswa("Arya Frandika Daulay","1302213069"),
+            new mahasiswa("Marsya Rayani Humaira","1302213003")
+        };
+
+            [HttpGet]
+            public IEnumerable<mahasiswa> Get()
+            {
+                return data;
+            }
+
+            [HttpGet("{id}")]
+            public mahasiswa Get(int id)
+            {
+                return data[id];
+            }
+            [HttpPost]
+            public void Post(mahasiswa value)
+            {
+                data.Add(value);
+            }
+
+            [HttpDelete("{id}")]
+            public void Delete(int id)
+            {
+                data.RemoveAt(id);
+            }
+        }
+    }
+}
---
+++ tp_mod9_1302210076/mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿namespace tp_mod9_1302210076
+{
+    public class mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+    }
+}
